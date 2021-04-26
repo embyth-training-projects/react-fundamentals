c647fd2e78641082ec2307c23635fea75b27043b
Rostyslav Miniukov <thresh1337@gmail.com>
2021-04-26 16:00:27 +0300

    Basic Todo app setup

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+function Todo() {
+  return (
+    <div className="card">
+      <h2>Title</h2>
+      <div className="actions">
+        <button className="btn" type="button">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
