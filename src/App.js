13d1258bd87b164b7a20fde7c03ec56795f01f0c
Rostyslav Miniukov <thresh1337@gmail.com>
2021-04-26 17:16:41 +0300

    Rebuilding project. React router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import Todo from "./components/Todo";
+import { Switch, Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Read react docs" />
-      <Todo text="Drink some coffee" />
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <AllMeetupsPage />
+      </Route>
+      <Route path="/favorites">
+        <FavoritesPage />
+      </Route>
+      <Route path="/new">
+        <NewMeetupPage />
+      </Route>
+    </Switch>
   );
 }
 
