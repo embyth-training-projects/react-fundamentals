e6e10d7f4ff3d29aa49f3ad54c841a13b67be45d
Rostyslav Miniukov <thresh1337@gmail.com>
2021-04-26 17:48:26 +0300

    Module styling and router navigation

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import { Switch, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
+import React from "react";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <AllMeetupsPage />
-      </Route>
-      <Route path="/favorites">
-        <FavoritesPage />
-      </Route>
-      <Route path="/new">
-        <NewMeetupPage />
-      </Route>
-    </Switch>
+    <React.Fragment>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+        <Route path="/new">
+          <NewMeetupPage />
+        </Route>
+      </Switch>
+    </React.Fragment>
   );
 }
 
