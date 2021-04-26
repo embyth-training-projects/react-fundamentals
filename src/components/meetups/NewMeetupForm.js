6ed69b4242730e9b25f618cd2ce0712173e8f1bc
Rostyslav Miniukov <thresh1337@gmail.com>
2021-04-26 19:41:44 +0300

    Adding Firebase test storage request for new meetup form

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,26 +1,56 @@
+import { useRef } from "react";
+
 import Card from "../ui/Card";
 
 import styles from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    const titleValue = titleInputRef.current.value;
+    const imageValue = imageInputRef.current.value;
+    const addressValue = addressInputRef.current.value;
+    const descriptionValue = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: titleValue,
+      image: imageValue,
+      address: addressValue,
+      description: descriptionValue,
+    };
+
+    props.onFormSubmit(meetupData);
+  }
+
   return (
     <Card>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={formSubmitHandler}>
         <div className={styles.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input id="title" type="text" required />
+          <input ref={titleInputRef} id="title" type="text" required />
         </div>
         <div className={styles.control}>
           <label htmlFor="image">Meetup Image Url</label>
-          <input id="image" type="url" required />
+          <input ref={imageInputRef} id="image" type="url" required />
         </div>
         <div className={styles.control}>
           <label htmlFor="address">Meetup Address</label>
-          <input id="address" type="text" required />
+          <input ref={addressInputRef} id="address" type="text" required />
         </div>
         <div className={styles.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" row="5" required />
+          <textarea
+            ref={descriptionInputRef}
+            id="description"
+            row="5"
+            required
+          />
         </div>
         <div className={styles.actions}>
           <button type="submit">Add Meetup</button>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,27 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function formSubmitHandler(meetupData) {
+    fetch(
+      "https://react-fundamentals-2cf0c-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(meetupData),
+      }
+    ).then(() => history.replace("/"));
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onFormSubmit={formSubmitHandler} />
     </section>
   );
 }
